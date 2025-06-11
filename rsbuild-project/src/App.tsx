5b52ba790ddc905f25fb51734e5c444a046467ae
tinhtranbk <tranvantinhbk@gmail.com>
2025-06-11 08:51:54 +0000

    update

---
+++ rsbuild-project/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Rsbuild TINH</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
